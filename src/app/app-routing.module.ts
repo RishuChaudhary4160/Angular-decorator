917bdb51d7c7adfb5dc8d38cac8e15a860fcec9f
Rishu <rishuchaudhary417@gmail.com>
2025-06-03 16:26:13 +0530

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SubjectComponent } from './subject/subject.component';
 import { ParentComponent } from './parent/parent.component';
+import { ServiceApiComponent } from './service-api/service-api.component';
+import { PurePipeComponent } from './pure-pipe/pure-pipe.component';
 
 const routes: Routes = [
   {path:"Subject",component:SubjectComponent},
-  {path:"Decorator",component:ParentComponent}
+  {path:"Decorator",component:ParentComponent},
+  {path:'service',component:ServiceApiComponent},
+  {path:'pipe',component:PurePipeComponent},
+
+  
 
 ];
 
